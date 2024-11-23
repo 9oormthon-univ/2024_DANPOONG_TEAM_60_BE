c6e8a179e39a1b0f7c409280ba2e04d5ecac51df
unknown <YaSic>
2024-11-23 16:23:50 +0900

    feat: 푸시알림 기능 추가

--- src/main/java/com/example/goo/Auth/config/SecurityConfig.java
+++ src/main/java/com/example/goo/Auth/config/SecurityConfig.java
@@ -6,11 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
-
 
 import java.io.IOException;
 
@@ -24,19 +20,16 @@ import java.util.Arrays;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Value("ed96b0600f58295622d71d0a86f7e1d6")
+    @Value("")
     private String clientId;
 
-    @Value("http://44.212.10.165:8080")
+    @Value("")
     private String redirectUri;
 
     private final CustomOAuth2UserService customOAuth2UserService;
-    private final OAuth2AuthorizedClientService authorizedClientService;
 
-    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService,
-                          OAuth2AuthorizedClientService authorizedClientService) {
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
         this.customOAuth2UserService = customOAuth2UserService;
-        this.authorizedClientService = authorizedClientService;
     }
 
     @Bean
@@ -44,7 +37,7 @@ public class SecurityConfig {
         http
 
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/login", "/api/logout", "/api/me").permitAll()
+                        .requestMatchers("/api/login", "/api/logout").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(oauth2 -> oauth2
@@ -52,23 +45,17 @@ public class SecurityConfig {
                                 .userService(customOAuth2UserService)
                         )
                         .successHandler((request, response, authentication) -> {
-                            // OAuth2 인증 객체에서 AccessToken 가져오기
-                            String accessToken = getAccessToken(authentication);
-
-                            // HTTP 헤더에 AccessToken 추가
-                            response.setHeader("Authorization", "Bearer " + accessToken);
-
-                            // 로그인 성공 후 리다이렉션
-                            response.sendRedirect("http://221.142.24.218:80/loginsuccess");
+                            // 로그인 성공 시 React 메인으로 리디렉션
+                            response.sendRedirect("http://localhost:3000/loginsuccess");
                         })
                         .failureHandler((request, response, exception) -> {
                             // 로그인 실패 시 React 로그인 실패 페이지로 리디렉션
-                            response.sendRedirect("http://221.142.24.218:80/loginfailed");
+                            response.sendRedirect("http://localhost:3000/loginfailed");
                         })
                 )
                 .logout(logout -> logout
                         .logoutUrl("/api/logout")
-                        .logoutSuccessUrl("http://221.142.24.218:80/logoutsuccess") // 로그아웃 성공 후 React로 리디렉션
+                        .logoutSuccessUrl("http://localhost:3000/logoutsuccess") // 로그아웃 성공 후 React로 리디렉션
                         .addLogoutHandler((request, response, authentication) -> {
                             String logoutUrl = "https://kauth.kakao.com/oauth/logout?client_id=" + clientId + "&logout_redirect_uri=" + redirectUri;
                             try {
@@ -84,21 +71,12 @@ public class SecurityConfig {
         return http.build();
     }
 
-    private String getAccessToken(Object authentication) {
-        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
-        OAuth2AuthorizedClient client = authorizedClientService.loadAuthorizedClient(
-                oauthToken.getAuthorizedClientRegistrationId(),
-                oauthToken.getName()
-        );
-        return client.getAccessToken().getTokenValue();
-    }
-
     // CORS 설정 추가
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://221.142.24.218:80")); // React 도메인
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 메서드
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // React 도메인
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE")); // 허용할 메서드
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type")); // 허용할 헤더
         configuration.setAllowCredentials(true); // 세션 쿠키 허용
 
@@ -106,4 +84,6 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration); // 모든 엔드포인트에 대해 CORS 적용
         return source;
     }
+
 }
+
--- src/test/java/com/example/goo/GooApplicationTests.java
+++ src/test/java/com/example/goo/GooApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.example.goo;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class GooApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.example.goo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
